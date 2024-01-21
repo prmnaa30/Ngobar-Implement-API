2cfc2ed676e54509b7237b1f3fcee34ec139dbe0
radespratama <puturades89@gmail.com>
2024-01-21 13:13:07 +0800

    feat: implement api get question

--- scripts/app.dashboard.js
+++ scripts/app.dashboard.js
@@ -1,8 +1,42 @@
 // == TEMPLATE QUIZ ITEM ==
 
-// generateQuizItem({
-//   id: question.id,
-//   question: question.jokes,
-//   answer: question.answer,
-//   category: question.category,
-// });
+
+
+import { getQuestions, createQuestion } from "./api.js";
+import { generateQuizItem } from "./utils/index.js";
+
+const quizContent = document.getElementById("quiz-content");
+
+document.addEventListener("DOMContentLoaded", () => {
+  async function handleAllQuestion() {
+    try {
+      // ARRAY []
+      const questions = await getQuestions();
+
+      // Kalo tidak ada data maka jangan kirim apapun
+      if (!questions) return;
+
+      quizContent.innerHTML = questions.map(
+        (question) => {
+
+          return generateQuizItem({
+            id: question.id,
+            question: question.jokes,
+            answer: question.answer,
+            category: question.category,
+          });
+        }
+      ).join("")
+
+      // Kalo data nya ada maka lanjut
+      console.log({ questions });
+    } catch (error) {
+      // Tangkap error jika gagal mengambil data di API
+      console.error("Ada error nih : ", {
+        error,
+      });
+    }
+  }
+
+  handleAllQuestion();
+});
