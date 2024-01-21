9de27a7e57478ddd3e441b90140300101c068163
prmnaa30 <73104057+prmnaa30@users.noreply.github.com>
2024-01-21 13:20:46 +0800

    update

--- scripts/app.dashboard.js
+++ scripts/app.dashboard.js
@@ -6,3 +6,36 @@
 //   answer: question.answer,
 //   category: question.category,
 // });
+
+import { getQuestions, createQuestion } from './api.js';
+import { generateQuizItem } from './utils/index.js'
+
+const quizContent = document.getElementById("quiz-content");
+
+document.addEventListener("DOMContentLoaded", () => {
+    async function handleAllQuestion() {
+        try {
+            const question = await getQuestions();
+
+            if (!question) return;
+
+            quizContent.innerHTML = question.map((question) => {
+                return generateQuizItem({
+                    id: question.id,
+                    question: question.jokes,
+                    answer: question.answer,
+                    category: question.category,
+                });
+            }
+            ).join("");
+
+            console.log({ question });
+
+        } catch (error) {
+            console.error("Error cuy" , {
+                error,
+            });
+        }
+    }
+    handleAllQuestion();
+});
